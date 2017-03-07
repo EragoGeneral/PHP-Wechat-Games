2bebee308d4174ed0c5546c71f46004debf9c5d6
EragoGeneral <lhr_nicelife@163.com>
2017-03-07 19:42:03 +0800

    add the detail page

---
+++ vote/vote/footer.php
@@ -0,0 +1,23 @@
+<div>
+	<ul style="width:100%; margin:0 auto;" class="bot_main" id="c_foot">
+		<li class="ico_2" id="foot_bu" style="width: 33%;">
+			<span class="ico">
+				<img src="images/daohang_02.png" width="20" height="20">
+			</span>
+			<span class="txt">搜索</span>
+		</li>	
+		<li class="ico_3" id="foot_bu" style="width: 33%;">
+			<a href="javascript:;" onclick="window.location.href = &quot;/index.php?g=Wap&amp;m=Voteimg&amp;a=vote_ranking&amp;id=7&amp;token=rowbhj1484111879&quot;;">
+			<span class="ico">
+				<img src="images/daohang_03.png" width="20" height="20"></span>
+				<span class="txt">排行</span>
+			</a>
+		</li>	
+		<li class="ico_4" id="foot_bu" style="width: 33%;">
+			<span class="ico">
+				<img src="images/daohang_04.png" width="20" height="20">
+			</span>
+			<span class="txt">拉票</span>
+		</li>  
+	</ul>
+</div>
