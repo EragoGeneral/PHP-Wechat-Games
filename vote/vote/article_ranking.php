837b16f06b6dcc92d77b01a4113358dd3501b204
EragoGeneral <lhr_nicelife@163.com>
2017-03-15 09:48:30 +0800

    update ranking page

--- vote/vote/article_ranking.php
+++ vote/vote/article_ranking.php
@@ -16,109 +16,35 @@ body {
 
 </head>
 <?php
-include 'header.php';
-?>
-<?php
-
-include 'tabs.php';
+    include 'header.php';
+    include 'tabs.php';
+    include 'dbconn.php';
+    
+    
 ?>
 <div class="blank20"></div>
 
 <div class="rank300" id="top300">
 	<ul>
-		<li class="rank-head"><span>排名</span><span>编号</span><span
-			style="display: inline-block; width: 40%">选项标题</span><span
-			style="width: 20%; color: #f67685">票数</span></li>
-		<li class="list"><span>1</span><span>150</span><span
-			style="display: inline-block; width: 40%">梦幻美少女</span><span
-			style="width: 20%; color: #f67685">6947</span></li>
-		<li class="list"><span>2</span><span>92</span><span
-			style="display: inline-block; width: 40%">充满幻想的世界</span><span
-			style="width: 20%; color: #f67685">5595</span></li>
-		<li class="list"><span>3</span><span>107</span><span
-			style="display: inline-block; width: 40%">冬、夏</span><span
-			style="width: 20%; color: #f67685">5365</span></li>
-		<li class="list"><span>4</span><span>121</span><span
-			style="display: inline-block; width: 40%">素描圣诞</span><span
-			style="width: 20%; color: #f67685">4556</span></li>
-		<li class="list"><span>5</span><span>118</span><span
-			style="display: inline-block; width: 40%">手指画-鸡年大吉</span><span
-			style="width: 20%; color: #f67685">1500</span></li>
-		<li class="list"><span>6</span><span>253</span><span
-			style="display: inline-block; width: 40%">手绘</span><span
-			style="width: 20%; color: #f67685">1372</span></li>
-		<li class="list"><span>7</span><span>27</span><span
-			style="display: inline-block; width: 40%">新春佳节</span><span
-			style="width: 20%; color: #f67685">1352</span></li>
-		<li class="list"><span>8</span><span>104</span><span
-			style="display: inline-block; width: 40%">鸡年大吉</span><span
-			style="width: 20%; color: #f67685">1283</span></li>
-		<li class="list"><span>9</span><span>163</span><span
-			style="display: inline-block; width: 40%">静悄悄的后院</span><span
-			style="width: 20%; color: #f67685">1213</span></li>
-		<li class="list"><span>10</span><span>198</span><span
-			style="display: inline-block; width: 40%">金鸡报晓</span><span
-			style="width: 20%; color: #f67685">1176</span></li>
-		<li class="list"><span>11</span><span>34</span><span
-			style="display: inline-block; width: 40%">笑口常开财源广进</span><span
-			style="width: 20%; color: #f67685">1130</span></li>
-		<li class="list"><span>12</span><span>14</span><span
-			style="display: inline-block; width: 40%">都市喵奇谭</span><span
-			style="width: 20%; color: #f67685">1012</span></li>
-		<li class="list"><span>13</span><span>52</span><span
-			style="display: inline-block; width: 40%">彩虹城 临摹</span><span
-			style="width: 20%; color: #f67685">948</span></li>
-		<li class="list"><span>14</span><span>21</span><span
-			style="display: inline-block; width: 40%">百鸟图百鸟朝凤</span><span
-			style="width: 20%; color: #f67685">816</span></li>
-		<li class="list"><span>15</span><span>164</span><span
-			style="display: inline-block; width: 40%">静</span><span
-			style="width: 20%; color: #f67685">812</span></li>
-		<li class="list"><span>16</span><span>106</span><span
-			style="display: inline-block; width: 40%">漫画精灵</span><span
-			style="width: 20%; color: #f67685">686</span></li>
-		<li class="list"><span>17</span><span>29</span><span
-			style="display: inline-block; width: 40%">海边</span><span
-			style="width: 20%; color: #f67685">595</span></li>
-		<li class="list"><span>18</span><span>207</span><span
-			style="display: inline-block; width: 40%">水粉和素描静物</span><span
-			style="width: 20%; color: #f67685">549</span></li>
-		<li class="list"><span>19</span><span>74</span><span
-			style="display: inline-block; width: 40%">圣诞水晶球</span><span
-			style="width: 20%; color: #f67685">540</span></li>
-		<li class="list"><span>20</span><span>83</span><span
-			style="display: inline-block; width: 40%">快乐的绘画男孩</span><span
-			style="width: 20%; color: #f67685">509</span></li>
-		<li class="list"><span>21</span><span>122</span><span
-			style="display: inline-block; width: 40%">毛绒玩具</span><span
-			style="width: 20%; color: #f67685">508</span></li>
-		<li class="list"><span>22</span><span>93</span><span
-			style="display: inline-block; width: 40%">面朝大海春暖花开</span><span
-			style="width: 20%; color: #f67685">440</span></li>
-		<li class="list"><span>23</span><span>3</span><span
-			style="display: inline-block; width: 40%">鸡年大吉大利</span><span
-			style="width: 20%; color: #f67685">422</span></li>
-		<li class="list"><span>24</span><span>188</span><span
-			style="display: inline-block; width: 40%">剪纸+白描</span><span
-			style="width: 20%; color: #f67685">404</span></li>
-		<li class="list"><span>25</span><span>130</span><span
-			style="display: inline-block; width: 40%">彩铅的世界</span><span
-			style="width: 20%; color: #f67685">374</span></li>
-		<li class="list"><span>26</span><span>87</span><span
-			style="display: inline-block; width: 40%">鹦鹉</span><span
-			style="width: 20%; color: #f67685">371</span></li>
-		<li class="list"><span>27</span><span>227</span><span
-			style="display: inline-block; width: 40%">风景静物</span><span
-			style="width: 20%; color: #f67685">328</span></li>
-		<li class="list"><span>28</span><span>51</span><span
-			style="display: inline-block; width: 40%">蝶恋花</span><span
-			style="width: 20%; color: #f67685">321</span></li>
-		<li class="list"><span>29</span><span>103</span><span
-			style="display: inline-block; width: 40%">妈妈是老虎</span><span
-			style="width: 20%; color: #f67685">300</span></li>
-		<li class="list"><span>30</span><span>26</span><span
-			style="display: inline-block; width: 40%">刮画，卡通</span><span
-			style="width: 20%; color: #f67685">290</span></li>
+		<li class="rank-head">
+		  <span>排名</span><span>编号</span>
+		  <span style="display: inline-block; width: 40%">选项标题</span>
+		  <span style="width: 18%; color: #f67685">票数</span>
+		</li>
+		<li class="list">
+		  <span>1</span><span>150</span>
+		  <span style="display: inline-block; width: 40%">梦幻美少女</span>
+		  <span style="width: 18%; color: #f67685">6947</span>
+		</li>
+		<li class="list">
+		  <span>2</span><span>92</span>
+		  <span style="display: inline-block; width: 40%">充满幻想的世界</span>
+		  <span style="width: 18%; color: #f67685">5595</span></li>
+		<li class="list">
+		  <span>3</span><span>107</span>
+		  <span style="display: inline-block; width: 40%">冬、夏</span>
+		  <span style="width: 18%; color: #f67685">5365</span>
+		</li>
 	</ul>
 </div>
 <div class="pagination pagination-centered" style="margin-top: 20px;">
@@ -130,8 +56,8 @@ include 'tabs.php';
 	<a
 		href="/index.php?g=Wap&amp;m=Voteimg&amp;a=vote_ranking&amp;id=7&amp;token=rowbhj1484111879&amp;mk=d542b3e8fb172a397d1d2da7e9decd21&amp;p=2">&gt;&gt;</a>
 </div>
-<div style=" height:60px; width:100%; display:block;"></div>
+<div style="height: 60px; width: 100%; display: block;"></div>
 <?php
-    include 'footer.php';
+include 'footer.php';
 ?>
 </html>
